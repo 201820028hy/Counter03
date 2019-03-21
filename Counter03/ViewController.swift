f74341a165766e1cf33750f66ac313ad02cf8aa4
dit03 <dit@imac-4.local>
2019-03-21 12:18:40 +0900

    start 0 end 9 and replay

--- Counter03/ViewController.swift
+++ Counter03/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbCount: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lbCount.text = "0"
     }
 
-
+    @IBAction func btnCount(_ sender: Any) {
+        count = count + 1
+        lbCount.text = String.init(count)
+        if(count == 10) {
+            count = 0
+            lbCount.text = "0"
+        }
+    }
+    
 }
 
